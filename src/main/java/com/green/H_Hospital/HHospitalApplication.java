455cc0339666c76dc705f2c95f47197b6c28015d
HCJIN <ghkdckswls02@naver.com>
2024-08-21 10:58:03 +0900

    git first upload

---
+++ src/main/java/com/green/H_Hospital/HHospitalApplication.java
@@ -0,0 +1,13 @@
+package com.green.H_Hospital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HHospitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/green/H_Hospital/HHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.H_Hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
