33a3a71903866cd524171e570da865d8db30c2fd
kong2bine <kong2bine@naver.com>
2024-08-22 16:16:32 +0900

    joinPage

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,27 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
 import './reset.css';
-<<<<<<< HEAD
 import { Route, Router, Routes, useNavigate } from 'react-router-dom';
 import Join from './component/Join';
 import JoinPage from './component/JoinPage';
+import Login from './component/Login';
 
 function App() {
 
   const navigate = useNavigate()
 
-=======
-import { Route, Router, Routes} from 'react-router-dom';
-import Login from './Login';
 
-function App() {
 
->>>>>>> ldh
   return (
     <div className="App">
       <div className='header'>
         <ul className='header-ul'>
-          <li>로그인</li>
+          <li onClick={()=>{navigate('/loginForm')}}>로그인</li>
           <li onClick={()=>{navigate('/MainJoin')}}>회원가입</li>
           <li>나의차트</li>
           <li>오시는길</li>
@@ -37,7 +31,6 @@ function App() {
 
 
       <Routes>
-<<<<<<< HEAD
         {/* 일반 유저용 */}
         <Route path='/'></Route>
         {/* 로그인 페이지 */}
@@ -45,9 +38,7 @@ function App() {
         {/* 회원가입 페이지 */}
         <Route path='Mainjoin' element={<Join />}></Route>
         <Route path='joinPage' element={<JoinPage />}></Route>
-=======
-        <Route path='/loginForm' element={<Login />}></Route>
->>>>>>> ldh
+        <Route path='loginForm' element={<Login />}></Route>
       </Routes>
 
       
