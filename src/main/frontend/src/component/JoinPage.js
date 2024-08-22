3050b8c738ce1c25ae1c56f7ab8b0778da74c93d
kong2bine <kong2bine@naver.com>
2024-08-22 15:16:41 +0900

    jhb

--- src/main/frontend/src/component/JoinPage.js
+++ src/main/frontend/src/component/JoinPage.js
@@ -1,8 +1,39 @@
-import React from 'react'
+import axios from 'axios';
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const JoinPage = () => {
+  const navigate = useNavigate();
+
+  //동의
+  const [agreeData, setAgreeData] = useState();
+  
+  axios.post('')
+  .then((res)=>{
+    
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+
+
   return (
-    <div>JoinPage</div>
+    <div>
+      <div>
+        <h3>이용악관</h3>
+          <textarea></textarea>
+          <input type='radio' name='agree1'/>동의함
+          <input type='radio' name='agree1'/>동의하지 않음
+        <h3>개인정보 수집 및 이용동의 (필수)</h3>
+          <textarea></textarea>
+          <input type='radio' name='agree2'/>동의함
+          <input type='radio' name='agree2'/>동의하지 않음
+      </div>
+      <div>
+        <button type='button'>동의합니다</button>
+        <button type='button' onClick={()=>{navigate('/')}}>동의하지 않습니다</button>
+      </div>
+    </div>
   )
 }
 
