b203b5c9984a5cc72780fc8fe794feaaff30e09a
HCJIN <ghkdckswls02@naver.com>
2024-08-29 14:51:40 +0900

    08.29

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -14,11 +14,8 @@ import MainPage from './component/MainPage';
 import { useEffect, useState } from 'react';
 import SuccessFindId from './component/SuccessFindId';
 import FindPw from './component/FindPw';
-<<<<<<< HEAD
 import Reservation from './component/Reservation';
-=======
 import AdminLogin from './component/AdminLogin';
->>>>>>> jbh
 
 function App() {
 
