373ebb42dd991074ac8a645cbbe2d909fe3c4d97
kong2bine <kong2bine@naver.com>
2024-08-29 15:16:04 +0900

    findId20240829

--- src/main/frontend/src/component/SuccessFindId.js
+++ src/main/frontend/src/component/SuccessFindId.js
@@ -4,7 +4,14 @@ const SuccessFindId = () => {
   return (
     <div>
       <div>아이디 찾기 성공</div>
-      <div></div>
+      <div>
+        <p>'회원이름'님의 아이디는</p>
+        <p>'findId'</p>
+        <p>입니다</p>
+      </div>
+      <div>
+        <button type='button'>로그인</button>
+      </div>
     </div>
   )
 }
