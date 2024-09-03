ecfa47dc3756bf7029b62fc37f2d2b8be896a526
kong2bine <kong2bine@naver.com>
2024-09-03 17:29:59 +0900

    240903

--- src/main/frontend/src/component/SuccessFindId.js
+++ src/main/frontend/src/component/SuccessFindId.js
@@ -1,39 +1,47 @@
-import '../css/SuccessFindId.css'
-import { useLocation, useNavigate } from 'react-router-dom'
+import '../css/SuccessFindId.css';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const SuccessFindId = () => {
-
-  //전달된 데이터 받아서 표시
   const location = useLocation();
-  
   const navigate = useNavigate();
-  
-  const { memInfo } = location.state;
+
+  // 위치에서 memInfo를 추출합니다. 안전하게 접근하기 위해 옵셔널 체이닝을 사용합니다.
+  const memInfo = location.state?.memInfo;
+
+  // 콘솔 로그로 데이터를 확인합니다.
+  console.log('Location State:', location.state);
+  console.log('MemInfo:', memInfo);
 
   return (
     <div className='successId-div'>
       <div className='successId-div-div'>
         <div className='successId-title'>아이디 찾기 완료</div>
         <div className='successId-message'>
-          <p>
-            <span>{memInfo.memName}</span>
-            회원님의 정보와 일치하는 아이디입니다
-          </p>
-          <table className='successId-table'>
-            <tbody>
-              <tr>
-                <td>"{memInfo.email}"</td>
-              </tr>
-            </tbody>
-          </table>
+          {memInfo ? (
+            <>
+              <p>
+                <span>{memInfo.memName}</span>
+                회원님의 정보와 일치하는 아이디입니다
+              </p>
+              <table className='successId-table'>
+                <tbody>
+                  <tr>
+                    <td>"{memInfo.email}"</td>
+                  </tr>
+                </tbody>
+              </table>
+            </>
+          ) : (
+            <p>입력한 정보와 일치하는 아이디가 없습니다</p>
+          )}
         </div>
       </div>
       <div className='successId-btn'>
-          <button type='button' onClick={()=>{navigate('/loginForm')}}>로그인 하기</button>
-          <button type='button' onClick={()=>{navigate('/findPw')}}>비밀번호 찾기</button>
-        </div>
+        <button type='button' onClick={() => navigate('/loginForm')}>로그인 하기</button>
+        <button type='button' onClick={() => navigate('/findPw')}>비밀번호 찾기</button>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SuccessFindId
+export default SuccessFindId;
