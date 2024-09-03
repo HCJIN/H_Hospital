865e8a8b1afac7514d97e92f799aac001036f477
dlfjs4585 <dlfjs4585@gmail.com>
2024-09-03 18:53:39 +0900

    patientChart update

--- src/main/frontend/src/component/PatientChart.js
+++ src/main/frontend/src/component/PatientChart.js
@@ -4,19 +4,43 @@ import axios from 'axios';
 
 const PatientChart = () => {
 
+  // 환자명을 검색했을 때 그 데이터를 저장 할 변수
+  const[selectName, setSelectName ] = useState('');
+
+  console.log(selectName)
+
+  // 환자명을 검색했을 때 값을 불러오는 함수
+  function changeSelectName(e){
+    setSelectName({
+      ...selectName,
+      [e.target.name] : e.target.value
+    });
+  }
+
   // 환자 한명의 정보를 조회한 데이터를 저장 할 변수
-  const [patienData, setPatienData] = useState();
+  const [patienData, setPatienData] = useState([]);
+
+  console.log(patienData)
 
   // 환자 한명의 정보를 조회하는 함수
   useEffect(() => {
-    axios.get('/admin/getPatientInfo')
+    axios.get('/chart/getPatientInfo')
     .then((res) => {
       setPatienData(res.data)
     })
     .catch((error) => {console.log(error)})
   }, [])
 
-  // 조회된 환자 정보를 검색 했을 때 나타나게 할 함수
+  // 조회된 환자 정보를 검색 했을 때 원하는 환자의 정보를 table에 출력할 함수
+
+  // 조회된 환자 정보를 검색 했을 때 그 환자의 모든 기록을 나타나게 할 함수
+  // function searchPatientChart(){
+  //   patienData.forEach((patien, i) => {
+  //     if(patien[i].memberV){
+
+  //     }
+  //   });
+  // }
 
   return (
     <div className='chart-div'>
@@ -24,7 +48,7 @@ const PatientChart = () => {
         {/* 이름을 검색했을 때 그에 상응하는 환자 검색 */}
         {/* 환자를 검색하지 않으면 조회된 데이터가 없다고 조건문으로 넣기 */}
         <div>
-          <input type='text' />
+          <input type='text' name='memName' onChange={(e) => {changeSelectName(e)}} />
           {/* 부트스트랩으로 검색 버튼 찾기 */}
         </div>
         {/* 환자 정보 이름, 휴대전화, 성별, 나이(나이는 년도를 기준으로 mapper에서 바꿀것) */}
@@ -72,6 +96,7 @@ const PatientChart = () => {
             <button type='button'>수정</button>
           </div>
         </div>
+        {/* 여기에 이름으로 검색한 환자의 모든 기록이 나오면 좋을 것 같음 */}
       </div>
     </div>
   )
--- src/main/java/com/green/H_Hospital/chart/controller/ChartController.java
+++ src/main/java/com/green/H_Hospital/chart/controller/ChartController.java
@@ -1,4 +1,22 @@
 package com.green.H_Hospital.chart.controller;
 
+import com.green.H_Hospital.chart.service.ChartService;
+import com.green.H_Hospital.chart.vo.ChartVO;
+import jakarta.annotation.Resource;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/chart")
+@RestController
 public class ChartController {
+
+    @Resource(name = "chartService")
+    private ChartService chartService;
+
+    @GetMapping("/getPatientInfo")
+    public List<ChartVO> getPatientInfo(){
+        return chartService.getPatientInfo();
+    }
+
 }
