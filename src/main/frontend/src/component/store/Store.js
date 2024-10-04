6e1acf7ad71b2400d03a6b68549736abb339c24e
kong2bine <kong2bine@naver.com>
2024-10-04 12:04:10 +0900

    1004hi

--- src/main/frontend/src/component/store/Store.js
+++ src/main/frontend/src/component/store/Store.js
@@ -14,10 +14,7 @@ const Store = () => {
   //제품 상세보기
   const [show, setShow] = useState(false);
 
-<<<<<<< HEAD
-  //선택된 카테고리 useState
-=======
->>>>>>> jhb
+
   const [selectedCategory, setSelectedCategory] = useState();
   //아이템 리스트 useState
   const [itemList, setItemList] = useState([]);
